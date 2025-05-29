c9620fe2123c8317a807cda9ae256957528724e1
Ronit650 <shayarihindi55@gmail.com>
2025-05-30 01:28:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-...
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to My Website</h1>
+      <p>This is a React site built using Vite!</p>
+    </div>
+  );
+}
+
+export default App;
